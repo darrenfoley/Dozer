1c09693f8bb89e7fdf7cb834c5f7682797c44bc7
Darren Foley <6538994+darrenfoley@users.noreply.github.com>
2020-06-10 23:03:04 -0600

    Fix 'colour' command
    -Pass arguments to Member.remove_roles correctly
    -Remove asyncio.Lock and just limit max_concurrency to 1 per guild
    -Clean up

--- cogs/general.py
+++ cogs/general.py
@@ -1,5 +1,4 @@
 import re
-from asyncio import Lock
 
 import discord
 from discord.ext import commands
@@ -11,15 +10,14 @@ from helpers.randomlists import EightBall
 class General(commands.Cog):
 
     def __init__(self, client):
-
         self._client = client
-        self._colour_lock = Lock()
 
     @commands.command(name='8ball')
     async def _8ball(self, ctx, *, question):
         await ctx.send(f'{ctx.message.author.mention}, {EightBall.random()}')
 
     @commands.command(name='colour', aliases=['color'])
+    @commands.max_concurrency(1, commands.BucketType.guild, wait=True)
     async def _colour(self, ctx, *, colour):
         colour_ci = colour.upper()
 
@@ -41,58 +39,37 @@ class General(commands.Cog):
             await ctx.send(
                 f'Invalid colour [{colour}] specified. Must be either a valid CSS3 colour or a hex colour (e.g. 0x00bfff). 0x000000 is not valid.')
         else:
-            async with self._colour_lock:
-                # find any existing colour roles and clean them up
-                # print(f'{colour} in lock')
-                user = ctx.message.author
-                user_roles = user.roles
-                already_has_role = False  # user already has the role they want
-                # print(f'roles before cleanup: [{ctx.guild.roles}]')
-                for r in user_roles:
-                    if r.name == colour_ci:
-                        already_has_role = True
-                        continue
-                    match = hex_colour_pattern.match(r.name)
-                    if match is not None:
-                        # Delete role if no one else is using it, else just remove the role from the user
-                        # print(f'role has {len(r.members)} members')
-                        num_members = len(r.members)
-                        if len(r.members) == 1:
-                            # print(f'start deleting role {r.name}')
-                            await r.delete(reason='unused colour role')
-                            # print(f'end deleting role {r.name}')
-                        else:
-                            # print(f'start removing role {r.name} from user')
-                            await user.remove_roles(r,
-                                                    f'user requested new colour role. Role still in use by {num_members - 1} others')
-                            # print(f'end removing role {r.name} from user')
+            # find any existing colour roles and clean them up
+            user = ctx.message.author
+            user_roles = user.roles
+            already_has_role = False  # user already has the role they want
+            for r in user_roles:
+                if r.name == colour_ci:
+                    already_has_role = True
+                    continue
+                match = hex_colour_pattern.match(r.name)
+                if match is not None:
+                    # Delete role if no one else is using it, else just remove the role from the user
+                    num_members = len(r.members)
+                    if num_members == 1:
+                        await r.delete(reason='unused colour role')
+                    else:
+                        await user.remove_roles(r,
+                                                reason=f'user requested new colour role. Role still in use by {num_members - 1} others')
 
-                    # print(f'roles after cleanup: [{ctx.guild.roles}]')
-                    # # Remove role from user
-                    # print(f'start removing role {r.name} from user')
-                    # await user.remove_roles(r)
-                    # print(f'end removing role {r.name} from user')
-                    # # Delete role if no one else is using it
-                    # print(f'role now has {len(r.members)} members')
-                    # if len(r.members) == 0:
-                    #   print(f'start deleting role {r.name}')
-                    #   await r.delete()
-                    #   print(f'end deleting role {r.name}')
+            if already_has_role:
+                return
 
-                if already_has_role:
-                    return
+            # check if role already exists
+            all_roles = ctx.guild.roles
+            role = next((x for x in all_roles if x.name == colour_ci), None)
+            if role is None:
+                # it does not exist. create it
+                colour_int = int(colour_ci, 16)
+                colour_obj = discord.Colour(colour_int)
+                role = await ctx.guild.create_role(name=colour_ci, colour=colour_obj)
 
-                # check if role already exists
-                all_roles = ctx.guild.roles
-                role = next((x for x in all_roles if x.name == colour_ci), None)
-                if role is None:
-                    # it does not exist. create it
-                    colour_int = int(colour_ci, 16)
-                    colour_obj = discord.Colour(colour_int)
-                    role = await ctx.guild.create_role(name=colour_ci, colour=colour_obj)
-
-                await user.add_roles(role)
-                # print(f'{colour} leaving lock')
+            await user.add_roles(role)
 
     @commands.command(name='unusedroles')
     @commands.is_owner()
